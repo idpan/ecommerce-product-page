ed64ebabd68dbb289beab95f430c5fbf053d8610
idpan <idpanashari813@gmail.com>
2022-04-13 15:59:41 +0700

    menambahkan image product

--- main.js
+++ main.js
@@ -18,9 +18,45 @@ closeBtn.onclick = () => {
     linkMenu.classList.remove("open-link");
   }, 500);
 };
-console.log(hamburgerBtn);
-
 // cart section toggle
 cartBtn.onclick = () => {
   cartContainer.classList.toggle("open");
 };
+
+// product image
+const imageForHero = ["image-product-1.jpg", "image-product-2.jpg", "image-product-3.jpg", "image-product-4.jpg"];
+const imageProduct = document.querySelector(".product-image__hero > img");
+const slider = document.querySelectorAll(".product-image__hero > button");
+const thunbnail = document.querySelectorAll(".product-image__thumbnail > button");
+let imageProductIndex = 0;
+slider.forEach((element) => {
+  element.onclick = function () {
+    imageProductIndex = setImageProductIndex(this.dataset.slide, imageProductIndex);
+
+    imageProduct.setAttribute("src", `./images/${imageForHero[imageProductIndex]}`);
+  };
+});
+
+thunbnail.forEach((element) => {
+  element.onclick = function () {
+    console.log(this);
+    imageProduct.setAttribute("src", `./images/${imageForHero[this.dataset.index]}`);
+    document.querySelector(".active-thumbnail").classList.remove("active-thumbnail");
+    this.classList.add("active-thumbnail");
+  };
+});
+
+function setImageProductIndex(slideButton, imageProductIndex) {
+  if (slideButton === "left") {
+    if (imageProductIndex === 0) {
+      return (imageProductIndex = imageForHero.length - 1);
+    }
+    return (imageProductIndex -= 1);
+  }
+  if (slideButton === "right") {
+    if (imageProductIndex === 3) {
+      return (imageProductIndex = 0);
+    }
+    return (imageProductIndex += 1);
+  }
+}
